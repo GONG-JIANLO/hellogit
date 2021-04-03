d7ee12723fd7b87a9fdf8f203f4ba505eaa5203f
gongjian <1060290034@qq.com>
2021-04-04 03:18:48 +0800

    添加文件

---
+++ gitWorkSpace/src/main/java/com/gongjian/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.gongjian.git;
+
+/**
+ * @author gong.j
+ * @create 2021-04-04 3:05
+ * @E-mail gj1060290034@163.com
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
