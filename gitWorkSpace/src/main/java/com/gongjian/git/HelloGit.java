085f862a317de3d6c02cc97fed4132fbe824c7ff
gongjian <1060290034@qq.com>
2021-04-04 13:08:34 +0800

    更新1

--- gitWorkSpace/src/main/java/com/gongjian/git/HelloGit.java
+++ gitWorkSpace/src/main/java/com/gongjian/git/HelloGit.java
@@ -8,5 +8,6 @@ package com.gongjian.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("版本一");
     }
 }
